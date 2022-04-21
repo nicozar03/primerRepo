52022e38692a4b36925461a6df3f529275d63ed2
URIEL ILAN <46213965@ort>
2022-04-21 10:08:24 -0300

    a

--- ejs/dai/tpBim1/01-express.js
+++ ejs/dai/tpBim1/01-express.js
@@ -1,35 +1,34 @@
 const express= require('express')
 const app=express();
 const port=3000;
-
-const process_data=()=>{
-    const data=[11,9,2,-1,110,99,8];
-    let odds=[];
-    for(let i =0; i<data.length; i++){
-        const element= data[i];
-        if(element % 2 !==0){
-            odds.push(element);
-        }
-    }
-    const proc=odds.sort().reverse();
-    console.log(proc[1]);
-    return {
-        resultado: proc[1]
-    };
-};
 app.use(express.json());
 
-app.post('/',(req,res)=>{
+function NumeroAleatorio(max){
+return Math.floor(Math.random()*(max-1))+1;
+}
+function crearCartones(num){
+}
 
-    console.log(req.body);
-    res.send(process_data(req.body));
-});
-app.get("/mi_endpoint", function(req,res){
-    res.send("respuesta");
-    });
-
-app.listen(port, function(err){
-    if (err) console.log(err);
-    console.log("Example app listenig on port",port)
+app.post("/numero_aleatorio", (req, res)=>{
+    console.log(req.body.numero);
+    let num =NumeroAleatorio(req.body.numero);
+    res.send([num]);
 });
 
+app.post("/iniciar_juego", (req, res)=>{
+    let arrayCartones=[];
+    for(let i=0; i< req.body.numero; i++){
+        let carton=[];
+        for(let i=0; i<10; i++){
+            let numero=NumeroAleatorio(100);
+
+            carton.push(numero);
+        }
+        arrayCartones.push(carton);
+    }
+    res.send([arrayCartones]);
+});
+
+app.listen(port, ()=>{
+    console.log(`example app listening on port ${port}`)
+});
