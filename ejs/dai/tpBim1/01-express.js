2959cc49314616be1b4d7306bd095be375e10bed
Android Studio <devandroid@ort>
2022-04-28 09:12:24 -0300

    S

--- ejs/dai/tpBim1/01-express.js
+++ ejs/dai/tpBim1/01-express.js
@@ -6,6 +6,8 @@ app.use(express.json());
 const NumeroAleatorio = max => Math.floor(Math.random()*(max-1))+1;
 var Cartones = {};
 
+
+
 app.post("/numero_aleatorio", (req, res)=>{
     console.log(req.body.numero);
     let num =NumeroAleatorio(req.body.numero);
@@ -18,6 +20,7 @@ app.post("/iniciar_juego", (req, res)=>{
         for(let j=0; j<10; j++)
             numerosCartones.push(NumeroAleatorio(100));
         Cartones[i] = numerosCartones;
+        /*Cartones.push(numerosCartones);*/
     }
     res.send(Cartones);
 });
@@ -31,12 +34,37 @@ app.get( "/obtener_carton", (req, res)=>{
 app.get( "/cartones", (req, res)=>{
     //let arrayPersonas=[]
     console.log(req.query.nombre);
-    if (Cartones[req.query.nombre]) res.send(Cartones[req.query.nombre])
+    if (Cartones[req.query.nombre]) {
+        res.send(Cartones[req.query.nombre])
+    }
     else{
-        res.send("not found");
+        res.send(Cartones);
     }
 
 });
+
+app.get("/sacar_numero"),(req,res)=>{
+        console.log ("me rindo");
+
+
+
+}
+
+
+/*app.get("/cartones/:nombre?", function(req,res){
+    let nombre=req.params.nombre;
+    console.log(req.params.nombre)
+    Cartones.find({nombre:nombre})
+    if (Cartones[req.params.nombre]==null){
+        res.send(Cartones);
+
+    }
+})*/
+
+
+
+
+
 
 
 app.listen(port, ()=>{
